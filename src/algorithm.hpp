1e45f75e317c36ef2c2e7aeac780b0f07cdb5a1e
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-01-12 17:15:42 +0100

    Add the entire algorithm
    Yes, the entirity of it
    I'm so good at splitting up smaller commits

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -1,2 +1,19 @@
 #pragma once
 
+namespace Algorithm {
+    typedef struct vec2 {
+        double x;
+        double y;
+    } vec2_t;
+
+    typedef struct sensor {
+        vec2_t position;
+        double duration;
+        bool measured;
+    } sensor_t;
+
+    typedef sensor_t* sorted_sensors_t;
+
+    bool calc_position(sensor_t sensors[3], vec2_t* out_position);
+}
+
