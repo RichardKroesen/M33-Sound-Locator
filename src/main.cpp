cb4e105b39d4b11c3521d746e14f4cf9de6e054c
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-01-12 15:14:44 +0100

    Fix clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include "pico/stdlib.h"
@@ -6,18 +7,18 @@
 #include "arm_math.h" // Check of CMSIS DSP inclusion
 
 void mainTask(void *params) {
-	printf("Boot task started\n");
-	gpio_init(PICO_DEFAULT_LED_PIN);
-	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    printf("Boot task started\n");
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-	for (;;) {
-		static int mytemptemptempVar = 430;
-		gpio_put(PICO_DEFAULT_LED_PIN, 1);
-		vTaskDelay(500);
+    for (;;) {
+        static int mytemptemptempVar = 430;
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        vTaskDelay(500);
         printf("Heartbeat...\n");
-		gpio_put(PICO_DEFAULT_LED_PIN, 0);
-		vTaskDelay(500);
-	}
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        vTaskDelay(500);
+    }
 }
 
 static inline void vLaunch() {
@@ -29,10 +30,10 @@ static inline void vLaunch() {
 }
 
 int main() {
-	/* Setup */
-	stdio_init_all();
+    /* Setup */
+    stdio_init_all();
     vLaunch();
 
-	while(1);
-	return 1;
+    while(1);
+    return 1;
 }
