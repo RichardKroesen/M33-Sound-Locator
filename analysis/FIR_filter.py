454d356bdaefd4bc4cae35361eefd927a467656f
Richard Kroesen <kroesen.ri@gmail.com>
2025-01-17 20:35:32 +0100

    Add FIR filter approach for clearcut filtering of the noise from our buggy HW.
    :

--- analysis/FIR_filter.py
+++ analysis/FIR_filter.py
@@ -1,40 +1,53 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.signal import firwin, lfilter
 from scipy.fft import fft, fftfreq
 
 # DATASET 
-data_file = '17125_measurements/FM_BF600/static.csv'  # Replace with your filename
+data_file = '17125_measurements/FM_BF600/BUZ-P20x20.csv'  # Replace with your filename
 df = pd.read_csv(data_file)
 
 # DEFINES
+NUM_TAPS = 51  # Number of filter coefficients
+PASS_BAND = 3995 
+BW = 5
 SAMPLE_RATE = (500_000 / 3)
-START_FROM = 30_000
-MAX_SPECTRUM = 1000
 
 # PRE PROCESSING
-df = df - df.mean()
-df_trimmed = df.iloc[START_FROM:len(df) - START_FROM]
-x = np.arange(len(df_trimmed))
+x = np.arange(len(df))
+
+bandpass_coefficients = firwin(
+    NUM_TAPS,
+    [
+        (PASS_BAND - BW) / (1/2 * SAMPLE_RATE),
+        (PASS_BAND + BW) / (1/2 * SAMPLE_RATE)
+    ],
+    pass_zero = False
+)
+
+df_filtered = df.copy()
+for column in ['RIGHT', 'MID', 'LEFT']:
+    df_filtered[column] = lfilter(bandpass_coefficients, 1.0, df[column])
 
 # VISUALIZE 
 plt.figure(figsize=(10, 6))
-plt.plot(x, df_trimmed['RIGHT'], label='RIGHT', marker='o')
-plt.plot(x, df_trimmed['MID'], label='MID', marker='o')
-plt.plot(x, df_trimmed['LEFT'], label='LEFT', marker='o')
+plt.plot(x, df_filtered['RIGHT'], label='RIGHT', marker='o')
+plt.plot(x, df_filtered['MID'], label='MID', marker='o')
+plt.plot(x, df_filtered['LEFT'], label='LEFT', marker='o')
 
 plt.xlabel('Sample Number(n)')
 plt.ylabel('Value')
 plt.title('Plot of RIGHT, MID, and LEFT Values (DC Removed)')
-plt.xlim(0, START_FROM)
+# plt.xlim(0,1000)
 plt.grid(True)
 plt.legend()
 plt.show()
 
 # Frequency Spectrum Analysis
-y1 = df_trimmed['RIGHT'].values
-y2 = df_trimmed['MID'].values
-y3 = df_trimmed['LEFT'].values
+y1 = df_filtered['RIGHT'].values
+y2 = df_filtered['MID'].values
+y3 = df_filtered['LEFT'].values
 
 n = len(y1)
 xf = fftfreq(n, 1 / SAMPLE_RATE)[:n // 2]
@@ -51,7 +64,7 @@ plt.plot(xf, 2.0/n * np.abs(yf3[:n // 2]), label = "L")
 plt.title(f'Frequency Spectrum (DC Removed)')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
-plt.xlim(5, MAX_SPECTRUM)
+# plt.xlim(5, MAX_SPECTRUM)
 plt.legend()
 plt.grid(True)
 plt.show()
