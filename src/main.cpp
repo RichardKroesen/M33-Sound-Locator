943bbb84198b10c866a77f118a5ff2697b101ad7
Richard Kroesen <kroesen.ri@gmail.com>
2025-01-19 02:06:25 +0100

    Enh cleanup of the codebase. Enh re-organized folders for more logical structure. Overall commit for improvement of maintainability.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
-#include <cstdio>
-#include "FreeRTOS.h"
-#include "task.h"
-#include "pico/stdlib.h"
+#include "main.hpp"
 
-#include <adc_driver_dma.hpp>
-#include <microphone_sensor.hpp>
-#include "arm_math.h"
-#include "adc_sample_distributor.hpp"
-#include "locator.hpp"
-#include "SampleProcessor.h"
+template <const size_t NUM_TASKS>
+static inline void vLaunch() {
+	BaseType_t xReturned[NUM_TASKS];
+	TaskHandle_t taskHandle[NUM_TASKS];
+
+    xReturned[0] = xTaskCreate(heart_beat_task, 
+		"Heartbeat", 
+		configMINIMAL_STACK_SIZE, 
+		NULL, tskIDLE_PRIORITY, &taskHandle[0]);
+
+	xReturned[1] = xTaskCreate(trigger_buz_on_button_task,
+		"BuzzerTrigger", 
+		configMINIMAL_STACK_SIZE, 
+		NULL, configMAX_PRIORITIES-2, &taskHandle[1]);
+
+	// Check the tasks are created, otherwise delete them:
+	for (size_t i = 0; i < NUM_TASKS; i++) {
+		if (xReturned[i] != pdPASS) {
+			vTaskDelete(taskHandle[i]);
+		}
+	}
+
+    /* Start the tasks and timer running. */
+    vTaskStartScheduler();
+}
 
 int main() {
 	/* Setup */
 	stdio_init_all();
+	vLaunch<1>();
+
     for (;;) {}
 	return 1;
 }
